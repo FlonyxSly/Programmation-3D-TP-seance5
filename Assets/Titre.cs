fd025fcd8643e3a019ef61dc205af6940149a3d7
Florian Leclercq <56671715+FlonyxSly@users.noreply.github.com>
2021-02-10 15:05:36 +0100

    TP Séance5

---
+++ Assets/Titre.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "NewTitle",
+menuName = "Titre")]
+public class Titre : ScriptableObject
+{
+    public string Title;
+}
